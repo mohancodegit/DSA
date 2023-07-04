7d141bc263be2a3d686211a363648b35a8c4cd9f
Mohan kumar A <mohancode30@gmail.com>
2023-07-04 16:27:53 +0530

    updated dsa code

--- factorial.js
+++ factorial.js
@@ -1,6 +1,6 @@
 function factoial(n) {
   let result = 1;
-  for (i = 2; i <= n; i++) {
+  for (let i = 2; i <= n; i++) {
     result *= i;
   }
   return result;
---
+++ powerOfTwo.js
@@ -0,0 +1,23 @@
+function powerOfTwo(n) {
+  if (n < 1) {
+    return false;
+  }
+  while (n > 1) {
+    if (n % 2 !== 0) {
+      return false;
+    }
+    n /= 2;
+  }
+  return true;
+}
+
+function powerOfwoWithBitwise(n) {
+  if (n < 1) {
+    return false;
+  }
+
+  //another use case
+  return (n & (n - 1)) === 0;
+}
+console.log(powerOfwoWithBitwise(15));
+console.log(powerOfwoWithBitwise(4));
